b332f399bb29044bda9d805e5441e5e6bfbddac0
ElijahButers <Elijahbuters@gmail.com>
2015-12-14 13:24:14 +0200

    Added code to tableView.

--- Swift2.0-HitListDemoApp/ViewController.swift
+++ Swift2.0-HitListDemoApp/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var names = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        title = "\"The List\""
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +26,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func addName(sender: AnyObject) {
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return names.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        cell?.textLabel?.text = names[indexPath.row]
+        return cell!
     }
 
 }
